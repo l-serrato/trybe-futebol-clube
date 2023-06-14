c35faa9b039e93a4ba8d2567eeb59099c6a9186f
Luis <l-serrato@live.com>
2023-06-14 14:47:52 -0300

    req13

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,57 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Match extends Model<InferAttributes<Match>,
+InferCreationAttributes<Match>> {
+  declare id: CreationOptional<number>;
+
+  declare homeTeamId: number;
+
+  declare homeTeamGoals: number;
+
+  declare awayTeamId: number;
+
+  declare awayTeamGoals: number;
+
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Match;
